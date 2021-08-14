160a4f19b5692e45e79b4d0117f8e61c773e201b
Interagus <adityan.sk@hotmail.com>
2021-08-14 11:31:06 +0530

    Rearrangement for hosting

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(express.urlencoded({extended:true}));
 const path = require("path");
 
 
-app.use(express.static(path.resolve(__dirname, "./oneshotcollege/build")));
 
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./oneshotcollege/build", "index.html"));
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
 
 app.listen(process.env.PORT||4000, ()=>{
     console.log("Listening")
